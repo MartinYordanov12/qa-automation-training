e559d616dbe32d8615a16d6f5addd8c731bdec9a
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-05 17:44:52 +0300

    Refactoring

--- src/test/java/com/yandex/Tests/BaseTest.java
+++ src/test/java/com/yandex/Tests/BaseTest.java
@@ -1,11 +1,9 @@
 package com.yandex.Tests;
 
 import com.yandex.pages.DriverSingleton;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.concurrent.TimeUnit;
 
