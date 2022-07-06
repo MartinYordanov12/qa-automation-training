76b4f809224429a651c99f798e42b4a73136ef00
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-06 11:19:54 +0300

    Refactoring

--- src/test/java/com/yandex/Tests/BaseTest.java
+++ src/test/java/com/yandex/Tests/BaseTest.java
@@ -2,15 +2,13 @@ package com.yandex.Tests;
 
 import com.yandex.pages.DriverSingleton;
 import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.WebDriver;
 
 import java.util.concurrent.TimeUnit;
 
 public class BaseTest {
-    protected static  WebDriver driver;
+    protected static WebDriver driver;
 
     @BeforeEach
     void setup() {
