e0a984ad39593facb7bafde7488292e207a828a9
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-06 16:10:24 +0300

    Fix comments of Vitaly Popkov

--- src/test/java/com/yandex/Tests/LoginTest.java
+++ src/test/java/com/yandex/Tests/LoginTest.java
@@ -2,25 +2,29 @@ package com.yandex.Tests;
 
 import com.yandex.pages.EmailPage;
 import com.yandex.pages.HomePage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class LoginTest extends BaseTest {
-    EmailPage emailPage = new EmailPage(driver);
-    HomePage homePage = new HomePage(driver);
+    public EmailPage emailPage;
+    public HomePage homePage;
 
-    @Test
-    @DisplayName("Login in mail.yandex.com and validate page open")
-    void loginTest() {
-        HomePage homePage = new HomePage(driver);
-        EmailPage emailPage = new EmailPage(driver);
+    @BeforeEach
+    void login() {
+        homePage = new HomePage(driver);
+        emailPage = new EmailPage(driver);
         homePage
                 .navigateToHomePage()
                 .clickOnLoginButton()
                 .login();
+    }
 
+    @Test
+    @DisplayName("Login in mail.yandex.com and validate page open")
+    void loginTest() {
         emailPage.clickOnDropdownMenu();
         assertTrue(emailPage.isEmailPageOpen());
     }
@@ -28,6 +32,7 @@ public class LoginTest extends BaseTest {
     @Test
     @DisplayName("Logout from mail.yandex.com and validate it")
     void logOut() {
+        emailPage.clickOnDropdownMenu();
         emailPage.clickOnLogOutButton();
         assertTrue(homePage.isLoginButtonPresented());
     }
