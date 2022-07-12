2086872cca68a7aa0337041a99e0c0abee0bfc2c
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-12 10:47:53 +0300

    apply Allure framework and add annotations

--- src/test/java/com/yandex/Tests/LoginTest.java
+++ src/test/java/com/yandex/Tests/LoginTest.java
@@ -2,11 +2,18 @@ package com.yandex.Tests;
 
 import com.yandex.pages.EmailPage;
 import com.yandex.pages.HomePage;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import jdk.jfr.Description;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Epic("Login Tests")
+@Feature("Login and logout tests")
 
 public class LoginTest extends BaseTest {
 
@@ -26,6 +33,8 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
+    @Story("User tries to login with valid credentials")
+    @Description("Valid login Test with valid username and password")
     @DisplayName("Login in mail.yandex.com and validate page open")
     void loginTest() {
         emailPage.clickOnDropdownMenu();
@@ -33,6 +42,8 @@ public class LoginTest extends BaseTest {
     }
 
     @Test
+    @Story("User tries to logout")
+    @Description("Click on logout button and verify that the user is logout")
     @DisplayName("Logout from mail.yandex.com and validate it")
     void logOut() {
         emailPage.clickOnDropdownMenu();
