4bfd54f444bbb25b214d1ef70e894b782b491425
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-13 14:31:19 +0300

    apply screenshot functionality on failed test

--- src/test/java/com/yandex/Tests/LoginTest.java
+++ src/test/java/com/yandex/Tests/LoginTest.java
@@ -2,13 +2,20 @@ package com.yandex.Tests;
 
 import com.yandex.pages.EmailPage;
 import com.yandex.pages.HomePage;
+import io.qameta.allure.Allure;
+import io.qameta.allure.AllureId;
 import io.qameta.allure.Epic;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import jdk.jfr.Description;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+import java.io.ByteArrayInputStream;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -35,6 +42,7 @@ public class LoginTest extends BaseTest {
     @Test
     @Story("User tries to login with valid credentials")
     @Description("Valid login Test with valid username and password")
+    @AllureId("1")
     @DisplayName("Login in mail.yandex.com and validate page open")
     void loginTest() {
         emailPage.clickOnDropdownMenu();
@@ -44,10 +52,13 @@ public class LoginTest extends BaseTest {
     @Test
     @Story("User tries to logout")
     @Description("Click on logout button and verify that the user is logout")
+    @AllureId("2")
     @DisplayName("Logout from mail.yandex.com and validate it")
     void logOut() {
         emailPage.clickOnDropdownMenu();
         emailPage.clickOnLogOutButton();
-        assertTrue(homePage.isLoginButtonPresented());
+        Allure.addAttachment("Yandex mail image",
+                new ByteArrayInputStream(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES)));
+        Assertions.assertEquals("Google", driver.getTitle());
     }
 }
