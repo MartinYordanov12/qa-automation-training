8a58eb74886a411df84e72f8379e067a11b28971
Martin Yordanov <MartinYordanov@coherentsolutions.com>
2022-07-20 12:22:09 +0300

    remove @DisplayName annotation

--- src/test/java/com/yandex/Tests/LoginTest.java
+++ src/test/java/com/yandex/Tests/LoginTest.java
@@ -10,7 +10,6 @@ import io.qameta.allure.Story;
 import jdk.jfr.Description;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
